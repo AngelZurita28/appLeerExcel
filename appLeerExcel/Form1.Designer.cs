fca03f5f025c54a5a41d60f1c2f72ee73b71e2e6
Angel Zurita <af3392976@gmail.com>
2022-05-25 00:02:51 -0500

    Add project files.

---
+++ appLeerExcel/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace appLeerExcel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvCeldas = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFiltrar = new System.Windows.Forms.TextBox();
+            this.btnFiltrar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnAbrir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCeldas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvCeldas
+            // 
+            this.dgvCeldas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCeldas.Location = new System.Drawing.Point(13, 86);
+            this.dgvCeldas.Name = "dgvCeldas";
+            this.dgvCeldas.Size = new System.Drawing.Size(775, 325);
+            this.dgvCeldas.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Filtrar Celdas";
+            // 
+            // txtFiltrar
+            // 
+            this.txtFiltrar.Location = new System.Drawing.Point(97, 26);
+            this.txtFiltrar.Name = "txtFiltrar";
+            this.txtFiltrar.Size = new System.Drawing.Size(569, 20);
+            this.txtFiltrar.TabIndex = 2;
+            this.txtFiltrar.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtFiltrar_KeyPress);
+            // 
+            // btnFiltrar
+            // 
+            this.btnFiltrar.Location = new System.Drawing.Point(672, 26);
+            this.btnFiltrar.Name = "btnFiltrar";
+            this.btnFiltrar.Size = new System.Drawing.Size(75, 23);
+            this.btnFiltrar.TabIndex = 3;
+            this.btnFiltrar.Text = "Filtrar";
+            this.btnFiltrar.UseVisualStyleBackColor = true;
+            this.btnFiltrar.Click += new System.EventHandler(this.btnFiltrar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(97, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(302, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Usar la siguiente sintaxis:   NombreDeColumna = \"valor de fila\"";
+            // 
+            // btnAbrir
+            // 
+            this.btnAbrir.Location = new System.Drawing.Point(12, 417);
+            this.btnAbrir.Name = "btnAbrir";
+            this.btnAbrir.Size = new System.Drawing.Size(75, 23);
+            this.btnAbrir.TabIndex = 5;
+            this.btnAbrir.Text = "Abrir";
+            this.btnAbrir.UseVisualStyleBackColor = true;
+            this.btnAbrir.Click += new System.EventHandler(this.btnAbrir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAbrir);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnFiltrar);
+            this.Controls.Add(this.txtFiltrar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvCeldas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCeldas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvCeldas;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFiltrar;
+        private System.Windows.Forms.Button btnFiltrar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnAbrir;
+    }
+}
+
---
+++ appLeerExcel/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClosedXML.Excel;
+
+namespace appLeerExcel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnFiltrar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataView dv = dgvCeldas.DataSource as DataView;
+                if (dv != null)
+                { dv.RowFilter = txtFiltrar.Text; }
+            }
+            catch (Exception ex)
+            { }
+        }
+        private void btnAbrir_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Excel Woorkbook |*.xlsx;";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                DataTable dataTable = new DataTable();
+                XLWorkbook wb = new XLWorkbook(openFileDialog.FileName);
+                bool isFirstRow = true;
+                var rows = wb.Worksheet(1).RowsUsed();
+                foreach (var row in rows)
+                {
+                    if (isFirstRow)
+                    {
+                        foreach (IXLCell cell in row.Cells())
+                            dataTable.Columns.Add(cell.Value.ToString());
+                        isFirstRow = false;
+                    }
+                    else
+                    {
+                        dataTable.Rows.Add();
+                        int i = 0;
+                        foreach (IXLCell cell in row.Cells())
+                        { dataTable.Rows[dataTable.Rows.Count - 1][i++] = cell.Value.ToString(); }
+                    }
+                }
+                dgvCeldas.DataSource = dataTable.DefaultView;
+            }
+        }
+        private void txtFiltrar_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            { btnFiltrar.PerformClick(); }
+        }
+    }
+}
